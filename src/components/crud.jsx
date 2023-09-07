76d5ec31f4ec37cd0ad0437d65e7a99bf943dfa9
Ernur3021 <ernuromarer@gmail.com>
2023-09-07 21:54:13 +0600

    fix bag

---
+++ src/components/crud.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+
+const Crud = () => {
+  return (
+    <div className='container'>
+        <div className='border__run margin_auto'>
+            <h1 className='first__title'>CRUD React</h1>
+            <p className='lorem__text margin_auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolorem labore in ullam architecto quos, modi odit est voluptas iste!</p>
+            <button className='first__btn'>Learn More</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Crud

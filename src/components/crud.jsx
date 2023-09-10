1a0773f82f63eccd412c8ce40d547037855fd953
Ernur3021 <ernuromarer@gmail.com>
2023-09-10 14:43:17 +0600

    add css class

--- src/components/crud.jsx
+++ src/components/crud.jsx
@@ -6,7 +6,13 @@ const Crud = () => {
         <div className='border__run margin_auto'>
             <h1 className='first__title'>CRUD React</h1>
             <p className='lorem__text margin_auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolorem labore in ullam architecto quos, modi odit est voluptas iste!</p>
-            <button className='first__btn'>Learn More</button>
+            <button className='first btn'>Learn More</button> 
+            <br/>
+            <div className='navbar'>
+                <button className='second btn'>Create User</button>
+                <input type='search' placeholder='Search...' className='search'></input>
+            </div> 
+
         </div>
 
     </div>
